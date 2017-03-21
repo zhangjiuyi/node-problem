7ec2e357e6aeb9b11a08189774ab74ba3aa61f08
张久屹 <447488560@qq.com>
2017-03-22 00:27:17 +0800

    dist

--- app/index.js
+++ app/index.js
@@ -1,26 +1,28 @@
 /**
  * Created by zhangjiuyi on 2017/3/18.
+ *主核心逻辑入口
+ *
  */
 
-
-//主核心逻辑文件
 	
+const fs = require('fs')
 
-	const fs = require('fs')
 class App {
 	constructor() {
 		
 	}
-	initServer() {
-		let _package  = require('../package')
-		
-		return (request, response)=>{
-			fs.readFile('./public/index.html','utf-8', (error, data)=>{
-				response.end(dataJSON.stringify(_package))
-			
+	initServer(){
+		//做一些初始化工作
+		let _package = require('../package')
+
+		//高阶函数.以函数做为函数的返回值
+		return (requset, response)=>{
+			fs.readFile('./public/index.html','utf8',(error,data)=>{
+				response.end(JSON.stringify(_package))
 			})
 		}
+		
 	}
 }
+module.exports = App
 
-moudle.exports = App
--- index.js
+++ index.js
@@ -1,35 +1,11 @@
 
 
-// var test = require('lodash')
-// console.log(test)
-//
-//
-// exports.hello = 'world';
-
-
-//核心模块
-//文件模块 一般是用户定义的模块
-
-// path, http, fs,
-
-
-// var another = require('./test').hello;
-// console.log(another)
-//
-//
-//
-// var package = require('./package')//node会自动帮添加后缀
-//
-// module.exports = {
-// 	// hello: hello,
-// 	a:
-// }
-
-const http = require('http');
+const http = require('http')
 const PORT = 7000
 const App = require('./app')
+
 const server = new App()
-http.createServer(server.initServer)
-.listen(7000,()=>{
-	console.log('server listening on port ${PORT}')
-})
+
+http.createServer(server.initServer()).listen(PORT,()=>{
+	console.log(`server listening on port ${PORT}`)
+});
